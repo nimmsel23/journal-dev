72e71395fe7ad86666b666f71bd3e880a1f23ed6
Alpha <alpha@alphaos.local>
2026-07-03 11:31:15 +0200

    simplify habits db: export * from @db

--- src/lib/db/habits.js
+++ src/lib/db/habits.js
@@ -1,6 +1,1 @@
-export {
-  getHabits, addHabit, deleteHabit, updateHabit,
-  recordHabit, unrecordHabit, getHabitRecordsForDate,
-  getHabitJournal, getHabitJournalHistory, getAllHabitJournalsHistory,
-  saveHabitJournal, localToday,
-} from '@db';
+export * from '@db';
